6c025de4ab8e11dccf56c62e81c096b877ffe0b1
Tomáš Klepač <74875406+tomasklepac@users.noreply.github.com>
2025-12-07 16:06:58 +0100

    Add map iframe title for accessibility

--- public/contact.php
+++ public/contact.php
@@ -68,7 +68,7 @@ include 'includes/header.php';
         </div>
 
         <div class="contact-map">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d1532.9679532561343!2d13.358797058758!3d49.74591276528011!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sTylova%202814%2F68%2C%20Plze%C5%88!5e0!3m2!1scs!2scz!4v1761433037333!5m2!1scs!2scz" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe title="Mapa Re-Move Harmony, Tylova 68, Plzeň" src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d1532.9679532561343!2d13.358797058758!3d49.74591276528011!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sTylova%202814%2F68%2C%20Plze%C5%88!5e0!3m2!1scs!2scz!4v1761433037333!5m2!1scs!2scz" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </div>
 </section>
